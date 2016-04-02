f0c17edefcc306d542733b40d3b1adfbc6d39c29
Tamas Domok <domoktams@gmail.com>
2016-04-02 11:00:41 +0200

    youtube.py: simplified and fixed the error handling.

--- src/zukebox/zukebox/youtube.py
+++ src/zukebox/zukebox/youtube.py
@@ -5,6 +5,7 @@ Copyright 2015, Tamas Domok
 Licensed under MIT.
 """
 
+from contextlib import contextmanager
 import youtube_dl
 import re
 
@@ -37,29 +38,41 @@ class Youtube:
     }
 
     def extract_info(self, youtube_url: str):
-        try:
+        with self.handle_errors():
             with youtube_dl.YoutubeDL(self.options) as ydl:
                 info = ydl.extract_info(youtube_url, download=False)
 
-                if not info or 'title' not in info:
-                    raise DownloadError("Could not extract song details; response='{response}'".format(response=str(info)))
+                def check_info():
+                    if not info:
+                        return False
+                    for check in ('title', 'duration', 'thumbnail'):
+                        if check not in info:
+                            return False
+                    return True
+
+                if not check_info():
+                    raise DownloadError("Could not extract song details; response='{response}'"
+                                        .format(response=str(info)))
 
                 return {
                     'title': info.get('title'),
                     'duration': info.get('duration'),
                     'thumbnail': info.get('thumbnails', [{}])[0].get('url', ''),
                 }
-        except youtube_dl.DownloadError as e:
-            raise DownloadError("Could not download song details; error='{error}'".format(error=str(e)))
-        except:
-            raise DownloadError("Could not download song details; error='unknown'")
 
     def download_audio(self, youtube_url: str, output: str):
-        try:
+        with self.handle_errors():
             options = self.options.copy()
             options['outtmpl'] = output
             with youtube_dl.YoutubeDL(options) as ydl:
                 ydl.download([youtube_url])
+
+    @contextmanager
+    def handle_errors(self):
+        try:
+            yield
+        except DownloadError:
+            raise
         except youtube_dl.DownloadError as e:
             raise DownloadError("Could not download song; error='{error}'".format(error=str(e)))
         except:
